a433f19aa0951acfc713a6a9d62ad69d11db7e3f
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-03-10 22:12:39 -0300

    feat: account entity

---
+++ src/main/java/br/senai/sc/models/Account.java
@@ -0,0 +1,29 @@
+package br.senai.sc.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
+@Entity
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private Integer registrationCode;
+
+    @ManyToOne
+    private User user;
+
+}
