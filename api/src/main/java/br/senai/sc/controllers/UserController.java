a9eff2229235c64b30048a7d9f2a8908321821a9
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-03-12 16:09:14 -0300

    feat: cors configuration

--- api/src/main/java/br/senai/sc/controllers/UserController.java
+++ api/src/main/java/br/senai/sc/controllers/UserController.java
@@ -22,6 +22,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@CrossOrigin
 public class UserController {
 
     private final UserService userService;
