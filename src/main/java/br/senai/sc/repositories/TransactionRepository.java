ddd47bd3ccad73b3929edebe65cb0fb4ce34354f
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-03-12 11:06:35 -0300

    fix: get account balance query

--- src/main/java/br/senai/sc/repositories/TransactionRepository.java
+++ src/main/java/br/senai/sc/repositories/TransactionRepository.java
@@ -14,7 +14,7 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
 
     @Query("SELECT SUM(COALESCE(t.value, 0)) "
             + "FROM Transaction t "
-            + "JOIN Account a on a.id = t.account.id "
+            + "RIGHT JOIN Account a on a.id = t.account.id "
             + "WHERE a.id = :accountId")
     Double getBalanceByAccountId(@Param("accountId") Long accountId);
 }
