f01ed3e0cb4ee8cb0f1c2463a54de27e0a0f3b05
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-03-12 12:09:00 -0300

    fix: find accounts by user query

--- api/src/main/java/br/senai/sc/repositories/AccountRepository.java
+++ api/src/main/java/br/senai/sc/repositories/AccountRepository.java
@@ -4,6 +4,7 @@ import br.senai.sc.dtos.AccountDTO;
 import br.senai.sc.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
             + "(a.id, a.registrationCode, a.user, SUM(COALESCE(t.value, 0))) "
             + "FROM Account a "
             + "LEFT JOIN Transaction t ON t.account.id = a.id "
+            + "WHERE a.user.id = :userId "
             + "GROUP BY a.id")
-    List<AccountDTO> findByUserId(Long userId);
+    List<AccountDTO> findByUserId(@Param("userId") Long userId);
 }
